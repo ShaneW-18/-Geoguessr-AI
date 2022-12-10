57c1da1286d520dbe99c02eb3b967821214a7a5d
shane <swiles201@gmail.com>
2022-12-10 04:02:41 -0500

    added get test data image to send

--- API/main.py
+++ API/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, Response
+from fastapi.responses import FileResponse
+import numpy as np
+import pickle as pkl
+import os
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 
@@ -11,3 +15,16 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get('/test-images')
+async def test_images():
+    base_dir = '../Model'
+    headers = {"Label": "My custom header value"}
+    imge_dir, label = get_random_image()
+    return FileResponse(os.path.join(base_dir, imge_dir), media_type='image/jpg')
+
+
+def get_random_image():
+    file_names = np.load('Test_file_names.npy', allow_pickle=True)
+    return file_names[0]
